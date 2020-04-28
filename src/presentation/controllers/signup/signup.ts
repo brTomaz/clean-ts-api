60acda6401828fa4102f48349535e2f3c0704e7c
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-04-28 00:18:37 -0300

    feat: ensure SignUpController returns 201 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -35,11 +35,16 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
+
+      return {
+        statusCode: 201,
+        body: account
+      }
     } catch (error) {
       return serverError()
     }
