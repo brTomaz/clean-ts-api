dc592462c4f6bf416499f4108bc4acf9121c69c9
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-05-14 14:24:13 -0300

    test: ensure MongoDB reconnect if client is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -3,20 +3,20 @@ import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-    })
-
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
-
-    beforeEach(async () => {
-      const accountCollection = MongoHelper.getCollection('accounts')
-      await accountCollection.deleteMany({})
-    })
-
     await request(app)
       .post('/api/signup')
       .send({
@@ -25,6 +25,6 @@ describe('Signup Routes', () => {
         password: 'any_password',
         passwordConfirmation: 'any_password'
       })
-      .expect(200)
+      .expect(201)
   })
 })
