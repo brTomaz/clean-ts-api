2a1c16e8d9a5ac342610cb4b903f81cab554502a
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-05-28 15:58:39 -0300

    refactor: modify .env configs

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,7 @@
+import 'dotenv/config'
+
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  mongoUrl: process.env.MONGO_URL,
+  port: process.env.PORT,
+  appUrl: process.env.APP_URL
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -3,5 +3,5 @@ import env from '../main/config/env'
 
 MongoHelper.connect(env.mongoUrl).then(async () => {
   const app = (await import('./config/app')).default
-  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  app.listen(env.port, () => console.log(`Server running at ${env.appUrl}`))
 }).catch(console.error)
