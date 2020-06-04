d941b9f45ec9997a2e1d459669e2575f74b1c9d1
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-06-03 23:46:38 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -9,31 +9,35 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return await new Promise(
-        resolve => resolve(
-          badRequest(new MissingParamError('email'))
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return await new Promise(
+          resolve => resolve(
+            badRequest(new MissingParamError('email'))
+          )
         )
-      )
-    }
+      }
 
-    if (!password) {
-      return await new Promise(
-        resolve => resolve(
-          badRequest(new MissingParamError('password'))
+      if (!password) {
+        return await new Promise(
+          resolve => resolve(
+            badRequest(new MissingParamError('password'))
+          )
         )
-      )
-    }
+      }
 
-    const isValidEmail = this.emailValidator.isValid(email)
+      const isValidEmail = this.emailValidator.isValid(email)
 
-    if (!isValidEmail) {
-      return await new Promise(
-        resolve => resolve(
-          badRequest(new InvalidParamError('email'))
+      if (!isValidEmail) {
+        return await new Promise(
+          resolve => resolve(
+            badRequest(new InvalidParamError('email'))
+          )
         )
-      )
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
