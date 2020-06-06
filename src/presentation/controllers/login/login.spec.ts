045b54b944f5dff43b730acf48cc62531598baf1
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-06-06 18:03:10 -0300

    refactor: group LoginController protocols in a single file

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,8 +1,11 @@
 import { LoginController } from './login'
-import { badRequest, serverError, unauthorizedError } from '../../helpers/http-helper'
+import {
+  badRequest,
+  serverError,
+  unauthorizedError
+} from '../../helpers/http-helper'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
-import { Authentication } from '../../../domain/usecases/authentication'
+import { EmailValidator, HttpRequest, Authentication } from './login-protocols'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,12 @@
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  EmailValidator,
+  Authentication
+} from './login-protocols'
 import { badRequest, serverError, unauthorizedError } from '../../helpers/http-helper'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { EmailValidator } from '../signup/signup-protocols'
-import { Authentication } from '../../../domain/usecases/authentication'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
