ac79f3d74110bf90577dfb4d17728fb65b0ff93b
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-06-14 01:05:13 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct ID

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,20 +1,24 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 import { HashComparator } from '../../protocols/cryptography/hash-comparator'
+import { TokenGenerator } from '../../protocols/cryptography/token-generator'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparator: HashComparator
+    private readonly hashComparator: HashComparator,
+    private readonly tokenGenerator: TokenGenerator
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparator = hashComparator
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
       await this.hashComparator.compare(authentication.password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
     return null
   }
