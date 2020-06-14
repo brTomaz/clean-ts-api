351c7c44de08cb9a86b9c5bfceea669b61d92db0
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-06-14 01:13:24 -0300

    test: ensure DbAuthentication returns an accessToken on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -132,4 +132,10 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an accessToken on success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
+  })
 })
