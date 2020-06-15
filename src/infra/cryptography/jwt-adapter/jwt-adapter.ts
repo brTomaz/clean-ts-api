38c55c354f20aaee7f0861068d6a59557769e2f8
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-06-15 02:15:19 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/cryptography/encrypter'
+
+export class JwtAdapter implements Encrypter {
+  constructor (private readonly secret: string) {
+    this.secret = secret
+  }
+
+  async encrypt (value: string): Promise<string> {
+    const accessToken = jwt.sign({ id: value }, this.secret)
+    return await Promise.resolve(accessToken)
+  }
+}
