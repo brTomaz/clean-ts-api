c8614a75afbd0b88acb8c7cf6df59aa5c3692bc2
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-06-15 01:46:31 -0300

    test: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -46,4 +46,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return true when compare succeeds', async () => {
+    const sut = makeSut()
+    const compare = await sut.compare('any_value', 'any_hash')
+    expect(compare).toBe(true)
+  })
 })
