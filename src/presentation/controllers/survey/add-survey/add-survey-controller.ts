bfe2e1ca0f6232a31ac294993587907ebdb70b71
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-06-26 17:53:31 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -4,7 +4,11 @@ import {
   HttpResponse,
   Validation
 } from './add-survey-controller-protocols'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import {
+  badRequest,
+  serverError,
+  noContent
+} from '../../../helpers/http/http-helper'
 import { AddSurvey } from '../../../../domain/usecases/add-survey'
 
 export class AddSurveyController implements Controller {
@@ -21,7 +25,7 @@ export class AddSurveyController implements Controller {
         return badRequest(validationError)
       }
       await this.addSurvey.add({ question, answers })
-      return null
+      return noContent()
     } catch (error) {
       return serverError(error)
     }
