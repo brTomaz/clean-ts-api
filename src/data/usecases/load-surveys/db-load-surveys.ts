ed3b8f692d59567c9256475c86f2acb2f239524b
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-07-13 16:17:56 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,12 @@
+import { LoadSurveys } from '../../../domain/usecases/load-surveys'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+import { SurveyModel } from '../../../domain/models/survey'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (private readonly loadSurveys: LoadSurveysRepository) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveys.loadAll()
+    return []
+  }
+}
